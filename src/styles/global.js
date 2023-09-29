b4348e6c830730f1e5ae8879f96a3c77903c0b1e
Lara Azevedo <lara.azevedo@outlook.com>
2023-09-29 12:36:25 -0300

    Medidas relativas e media queries

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import { DEVICE_BREAKPOINTS } from "./deviceBreakpoints";
 
 export default createGlobalStyle`
   * {
@@ -6,6 +7,15 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :root {
+    font-size: 16px;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      font-size: 12px;
+    }
+  }
+
 
   body {
     background-color: ${({ theme }) => theme.COLORS.GRAY_800};
@@ -16,7 +26,7 @@ export default createGlobalStyle`
 
   body, input, button, textarea {
     font-family: 'Roboto', sans-serif;
-    font-size: 16px;
+    font-size: 1rem;
     outline: none;
   }
 

bfa01544857e3db2b3b6fd93ba147bc493eec8b2
Lara Azevedo <lara.azevedo@outlook.com>
2023-09-29 13:10:52 -0300

    Deixando os componentes Priorities e ShortTask responsivos

--- src/components/Priorities/styles.js
+++ src/components/Priorities/styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.section`
-  padding: 32px;
+  padding: 2rem;
 `;
 
 export const Header = styled.header`
@@ -10,7 +11,7 @@ export const Header = styled.header`
   align-items: center;
 
   > h1 {
-    font-size: 24px;
+    font-size: 1.5rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
     display: flex;
     align-items: center;
@@ -23,13 +24,18 @@ export const Header = styled.header`
 `;
 
 export const Tasks = styled.div`
-  border: 1px dashed  ${({ theme }) => theme.COLORS.RED_200};
+  border: 1px dashed ${({ theme }) => theme.COLORS.RED_200};
   min-height: 154px;
-  border-radius: 7px;
+  border-radius: 0.43rem;
   margin-top: 16px;
 
   display: flex;
-  gap: 16px;
+  gap: 1rem;
   padding: 24px;
   flex-wrap: wrap;
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    justify-content: center;
+    align-items: center;
+  }
 `;
--- src/components/ShortTask/styles.js
+++ src/components/ShortTask/styles.js
@@ -5,16 +5,16 @@ export const Container = styled.button`
   color: ${({ theme }) => theme.COLORS.GRAY_100};
   height: 44px;
   border: 0;
-  padding: 0 16px;
-  border-radius: 7px;
-  font-size: 14px;
+  padding: 0 1rem;
+  border-radius: 0.43rem;
+  font-size: 0.87rem;
   
   display: flex;
   align-items: center;
   gap: 7px;
 
   > svg {
-    font-size: 18px;
+    font-size: 1.12rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
   }
 `;
